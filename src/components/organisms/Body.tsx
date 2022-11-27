dd9b5d7f8e9bc1deb60d8f9cafce76974e722736
--replace-all <gyuwonlee.olivia@gmail.com>
2022-11-27 14:58:25 +0900

    [ADD] API 응답 상태에 따른 로딩, 에러 페이지 추가

--- src/components/organisms/Body.tsx
+++ src/components/organisms/Body.tsx
@@ -6,6 +6,7 @@ import { courseInfo } from '../../types/apiDto';
 import useCourses from '../../hooks/useCourses';
 import Results from './Results';
 import Pagination from './Pagination';
+import EmptyResult from '../../pages/EmptyResult';
 
 const ResultContainer = styled.div`
   display: flex;
@@ -24,7 +25,11 @@ const Body = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [offset, setOffset] = useState(0);
   const search = searchParams.toString();
-  const { courses, setCourses, course_count } = useCourses(search, offset);
+  const { courses, setCourses, course_count, status } = useCourses(
+    search,
+    offset,
+  );
+  console.log(status);
 
   useEffect(() => {
     setOffset(0);
@@ -33,12 +38,39 @@ const Body = () => {
   return (
     <ResultContainer>
       <TotalResult>전체 {course_count}개</TotalResult>
-      <Results courses={courses} />
-      <Pagination
-        offset={offset}
-        setOffset={setOffset}
-        course_count={course_count}
-      />
+      {status === 'ok' ? (
+        course_count > 0 ? (
+          <>
+            <Results courses={courses} />
+            <Pagination
+              offset={offset}
+              setOffset={setOffset}
+              course_count={course_count}
+            />
+          </>
+        ) : (
+          <EmptyResult text="검색 결과가 없습니다." />
+        )
+      ) : status === 'loading' ? (
+        <EmptyResult text="로딩 중입니다." />
+      ) : (
+        <EmptyResult text="에러가 발생했습니다." />
+      )}
+
+      {/* {course_count > 0 ? (
+        <>
+          <Results courses={courses} />
+          <Pagination
+            offset={offset}
+            setOffset={setOffset}
+            course_count={course_count}
+          />
+        </>
+      ) : status === 'ok' ? (
+        <EmptyResult text="검색 결과가 없습니다." />
+      ) : (
+        <EmptyResult text="에러가 발생했습니다." />
+      )} */}
     </ResultContainer>
   );
 };
